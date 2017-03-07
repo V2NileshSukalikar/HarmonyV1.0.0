eec48a7247a18f77f8ebfc12137a09b06eae9f11
v2chandrakant <chandrakant.mane@v2solutions.com>
2017-03-07 18:30:38 +0530

    config
    config

--- DynamicApp/src/app/models/config.ts
+++ DynamicApp/src/app/models/config.ts
@@ -10,7 +10,8 @@ export class Config {
         this.headers = new Headers({ 'Content-Type': 'application/json' });
 //<<<<<<< HEAD
         this.apiURl = 'http://harmonyservice/api/CMSData/GetCMSJsonData';  // URL to web api
-          this.Searchurl = 'http://harmonyservice/api/CMSData/GetFilteredRecords';  // URL to web api
+          this.Searchurl = 'http://harmonyservice/api/CMSData/GetFilteredRecords'; 
+           // URL to web api
 // =======
 //         this.apiURl = 'http://localhost:53142/api/CMSData/GetcmsData';  // URL to web api
 // >>>>>>> refs/remotes/origin/master
