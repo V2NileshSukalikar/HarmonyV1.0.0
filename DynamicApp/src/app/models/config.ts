2f34758f38bb1199115a9c56bbdc3c13725a34ca
v2chandrakant <chandrakant.mane@v2solutions.com>
2017-03-07 18:10:02 +0530

    search result
    search result

--- DynamicApp/src/app/models/config.ts
+++ DynamicApp/src/app/models/config.ts
@@ -4,11 +4,13 @@ export class Config {
 
     public headers: Headers;
     public apiURl: string;
+     public Searchurl: string;
 
     constructor() {
         this.headers = new Headers({ 'Content-Type': 'application/json' });
 //<<<<<<< HEAD
-        this.apiURl = 'http://localhost:53142/api/CMSData/GetCMSJsonData';  // URL to web api
+        this.apiURl = 'http://harmonyservice/api/CMSData/GetCMSJsonData';  // URL to web api
+          this.Searchurl = 'http://harmonyservice/api/CMSData/GetFilteredRecords';  // URL to web api
 // =======
 //         this.apiURl = 'http://localhost:53142/api/CMSData/GetcmsData';  // URL to web api
 // >>>>>>> refs/remotes/origin/master
